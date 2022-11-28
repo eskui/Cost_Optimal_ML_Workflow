8fa0654e439a9bb61ce5c9982bb7d95a70572d22
eskui <esa.kuivaniemi@gmail.com>
2022-11-28 14:50:21 +0200

    updates

--- support/paleo_estimator.py
+++ support/paleo_estimator.py
@@ -1,12 +1,23 @@
 import paleo.profiler as profilers
 import paleo.device as device
 
+"""
+device.Device args:
+    name: device name
+    clock: MHz
+    peek_gflop: GFLOPS
+    mem_bandwidth: GB/sec
+"""
+
 NETWORK = device.Network("EC2_network", 20)
-DEVICE_T4GPU = device.Device("NVIDIA_T4",585,8100,320,True)
 DEVICE_LOCAL = device.Device("local_CPU",2300,40,2133,False)
+DEVICE_A10GPU = device.Device("G5_A10GPU",885,31240,600,True)
+DEVICE_T4GPU = device.Device("G4DN_T4GPU",585,8100,320,True)
 
 def estimate_required_time(device_name):
-    if device_name == "NVIDIA_T4":
+    if device_name == "G5_A10GPU":
+        device = DEVICE_A10GPU
+    elif device_name == "G4DN_T4GPU":
         device = DEVICE_T4GPU
     elif device_name == "local_CPU":
         device = DEVICE_LOCAL
--- train_init_script.sh
+++ train_init_script.sh
@@ -7,5 +7,6 @@ bash Miniconda3-latest-Linux-aarch64.sh
 aws s3 cp --recursive s3://ejkquant-uswest1/train_data ./train_data
 pip install pandas pyarrow matplotlib click
 git clone https://github.com/eskui/Cost_Optimal_ML_Workflow.git
-python3 train_model.py 2017 1440 NVIDIA_T4 16000
+python3 train_model.py 2017 1440 G5_A10GPU 16000
+python3 train_model.py 2017 1440 G4DN_T4GPU 16000
 python3 train_model.py 2017 1440 local_CPU 50
